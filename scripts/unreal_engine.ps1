71f243b70a5f437e5f29b23b0a651851e4361bb7
alexholliz <alex.holliz@gmail.com>
2021-05-28 16:17:35 -0700

    Swapped secret for environment variable for unreal engine, and added debug lines to unreal pull

--- scripts/unreal_engine.ps1
+++ scripts/unreal_engine.ps1
@@ -2,6 +2,7 @@ $ErrorActionPreference = 'Stop'
 
 try {
   Write-Host "Pulling down Unreal Engine version $env:UNREAL_ENGINE_VERSION from S3"
+  Write-Host "Command to run: aws s3 sync $env:S3_BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
   aws s3 sync $env:S3_BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
   Get-ChildItem -Path C:\Unreal\ -Name
 }
