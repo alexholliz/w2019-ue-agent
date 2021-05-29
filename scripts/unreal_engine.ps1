6bad153155f898af61f5efa852ca24705252c536
alexholliz <alex.holliz@gmail.com>
2021-05-28 17:23:09 -0700

    more env variable fixes

--- scripts/unreal_engine.ps1
+++ scripts/unreal_engine.ps1
@@ -9,7 +9,7 @@ try {
   Import-Module AWSPowerShell
 
   Write-Host "Pulling down Unreal Engine version $env:UNREAL_ENGINE_VERSION from S3"
-  Write-Host "Command to run: aws s3 sync $env:S3_BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
+  Write-Host "Command to run: aws s3 sync $env:BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
   aws s3 sync $env:BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
   Get-ChildItem -Path C:\Unreal\ -Name
 }
