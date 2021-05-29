365d09a749443459fa17cafdecb0f831da2f14f8
alexholliz <alex.holliz@gmail.com>
2021-05-29 13:35:28 -0700

    switching back to the awscli thanks to chocolatey

--- scripts/unreal_engine.ps1
+++ scripts/unreal_engine.ps1
@@ -1,19 +1,18 @@
 $ErrorActionPreference = 'Stop'
 
 try {
-  Write-Host "Importing AWSPowerShell Module"
+  Write-Host "Installing AWS CLI"
 
-  # Install AWS CLI
-  Import-Module AWSPowerShell
+  choco install awscli
 
   Set-Location -Path "C:\"
   mkdir Unreal
   mkdir Unreal\$env:UNREAL_ENGINE_VERSION
 
   Write-Host "Pulling down Unreal Engine version $env:UNREAL_ENGINE_VERSION from S3"
-  Write-Host "Command to run: Copy-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION 'C:\Unreal\$env:UNREAL_ENGINE_VERSION'"
-  # aws s3 sync s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
-  Copy-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION -LocalFolder C:\Unreal\$env:UNREAL_ENGINE_VERSION
+  Write-Host "aws s3 sync s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
+  aws s3 ls s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION
+  aws s3 sync s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\$env:UNREAL_ENGINE_VERSION
 
   Get-ChildItem -Path C:\Unreal\ -Name
 }
