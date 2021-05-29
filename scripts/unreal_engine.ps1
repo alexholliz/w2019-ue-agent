fd48bcbc502f3579ad2a65aac9e50bdb08356b40
alexholliz <alex.holliz@gmail.com>
2021-05-29 11:10:00 -0700

    using AWSPowerShell Commands

--- scripts/unreal_engine.ps1
+++ scripts/unreal_engine.ps1
@@ -4,15 +4,19 @@ try {
   Write-Host "Importing AWSPowerShell Module"
 
   # Install AWS CLI
-  msiexec.exe /i https://awscli.amazonaws.com/AWSCLIV2.msi /passive
+  Import-Module AWSPowerShell
 
   Set-Location -Path "C:\"
   mkdir Unreal
 
   Write-Host "Pulling down Unreal Engine version $env:UNREAL_ENGINE_VERSION from S3"
   Write-Host "Command to run: aws s3 sync $env:BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
-  aws s3 ls $env:BUCKET/$env:UNREAL_ENGINE_VERSION
-  aws s3 sync $env:BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
+  # aws s3 ls s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION
+  Get-S3Object -BucketName $env:BUCKET $env:UNREAL_ENGINE_VERSION
+
+  # aws s3 sync s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
+  Read-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION 'C:\Unreal\$env:UNREAL_ENGINE_VERSION'
+
   Get-ChildItem -Path C:\Unreal\ -Name
 }
 
