cba1ef0f02ed08657a672e3cb88b172fa4941cff
alexholliz <alex.holliz@gmail.com>
2021-05-29 12:45:32 -0700

    changing to Copy-S3Object

--- scripts/unreal_engine.ps1
+++ scripts/unreal_engine.ps1
@@ -8,14 +8,12 @@ try {
 
   Set-Location -Path "C:\"
   mkdir Unreal
+  mkdir Unreal\$env:UNREAL_ENGINE_VERSION
 
   Write-Host "Pulling down Unreal Engine version $env:UNREAL_ENGINE_VERSION from S3"
-  Write-Host "Command to run: aws s3 sync $env:BUCKET/$env:UNREAL_ENGINE_VERSION C:\Unreal\"
-  # aws s3 ls s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION
-  Get-S3Object -BucketName $env:BUCKET $env:UNREAL_ENGINE_VERSION
-
+  Write-Host "Command to run: Read-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION 'C:\Unreal\$env:UNREAL_ENGINE_VERSION'"
   # aws s3 sync s3://$env:BUCKET/$env:UNREAL_ENGINE_VERSION "C:\Unreal\"
-  Read-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION 'C:\Unreal\$env:UNREAL_ENGINE_VERSION'
+  Copy-S3Object -BucketName $env:BUCKET -KeyPrefix $env:UNREAL_ENGINE_VERSION C:\Unreal\$env:UNREAL_ENGINE_VERSION
 
   Get-ChildItem -Path C:\Unreal\ -Name
 }
