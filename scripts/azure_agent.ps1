f7ee52fb8c7c128b603fb762214499c50c381344
alexholliz <alex.holliz@gmail.com>
2021-06-01 17:39:28 -0700

    adding debug to azure-agent install

--- scripts/azure_agent.ps1
+++ scripts/azure_agent.ps1
@@ -3,16 +3,27 @@ $ErrorActionPreference = 'Stop'
 try {
 
   # Download and prep agentdir
+  Write-Host "Downloading Azure Agent"
   Invoke-WebRequest -Uri https://vstsagentpackage.azureedge.net/agent/2.187.1/vsts-agent-win-x64-2.187.1.zip -OutFile ~/Downloads/vsts-agent-win-x64-2.187.1.zip
+  
+  Write-Host "Switching to C:\"
   Set-Location - Path "C:\"
-  mkdir agent ; Set-Location -Path "C:\agent"
+
+  Write-Host "Making directory C:\agent"
+  mkdir agent
+
+  Write-Host "Switching to C:\agent"
+  Set-Location -Path "C:\agent"
+
+  Write-Host "Unzipping Azure Agent to $PWD"
   Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.187.1.zip", "$PWD")
 
   # Install agent non-interactively
+  Write-Host "Installing Azure Agent non-interactively"
   .\config --unattended --url $env:URL --auth pat --token $env:PAT --pool $env:POOL --agent $env:NAME --replace --runAsService
 }
 
 catch {
-  Write-Error "Failed to disable UAC"
+  Write-Error "Failed to Install Azure Agent"
   exit 1
 }
